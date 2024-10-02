337957f5f00c72f0e6b4a2d4f3e5970fefaa5a39
cheri <cheri@CherieLiu.dhcp.wustl.edu>
2024-10-02 14:12:25 -0500

    Finished making a flag

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,11 +1,29 @@
 package studio4;
 
 import java.awt.Color;
+import java.util.Scanner;
 
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		StdDraw.clear();
+		StdDraw.setPenColor(228, 183, 242);
+		StdDraw.filledRectangle(.5, .833, .5, .167);
+		StdDraw.setPenColor(255, 220, 241);
+		StdDraw.filledRectangle(.5, .5, .5, .167);
+		StdDraw.setPenColor(220, 255, 224);
+		StdDraw.filledRectangle(.5, .167, .5, .167);
+		StdDraw.setPenColor(254, 255, 220);
+		StdDraw.filledEllipse(.25, .75, .12, .12);
+		StdDraw.filledEllipse(.75, .75, .12, .12);
+		StdDraw.filledRectangle(.5, .33, .25, .07);
+	
+	
+		
+		
 		
 	}
 }
